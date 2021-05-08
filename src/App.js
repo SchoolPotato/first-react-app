acf02da195828539e710231ae026e6db1ab50c91
Alexander Welton <Awelto389@west-mec.local>
2021-05-07 18:16:28 -0700

    Finished UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -27,7 +28,9 @@ const App = () => {
 
   // Add Task
   const addTask = (task) => {
-    console.log(task)
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask]);
   }
 
   // Delete Task
@@ -42,8 +45,8 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "You're all caught up!"}
     </div>
   );
