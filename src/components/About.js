5f8a85df7030a8c1fa92ef522876b25162a95876
Alexander Welton <Awelto389@west-mec.local>
2021-05-10 17:45:13 -0700

    Personalized About page

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,10 @@ const About = () => {
     return (
         <div>
             <h4>Version 1.0.0</h4>
+            <p><strong>Name:</strong> Alexander J. Welton</p>
+            <p><strong>Date of Release:</strong> 5/10/2021</p>
+            <p><strong>Favorite Musician:</strong> Either Fearofdark or A.L.I.S.O.N</p>
+            <br></br>
             <Link to="/">Reminders</Link>
         </div>
     )
